987a19ffdef6fe16a54bae7940fa87ccf2b40578
Usuario <Usuario@INVENT081>
2023-10-30 10:49:56 -0300

    Adicionar arquivos de projeto.

---
+++ triangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        decimal A = Convert.ToDecimal(Console.ReadLine());
+        decimal B = Convert.ToDecimal(Console.ReadLine());
+        decimal C = Convert.ToDecimal(Console.ReadLine());
+
+        decimal perimetro = A + B + C;
+        decimal area = ((A + B) * C) / 2;
+
+        if (A < B + C && B < A + C && C < A + B)
+        {
+            Console.WriteLine("Perimetro = " + perimetro);
+        }
+        else
+        {
+            Console.WriteLine("Area = " + area);
+        }
+
+    }
+
+}
